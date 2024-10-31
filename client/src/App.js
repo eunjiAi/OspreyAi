4287b0bd8192da81326e4ea39d1cac6fbd5fe4cc
ieunji0116 <136577730+ieunji0116@users.noreply.github.com>
2024-10-31 17:51:51 +0900

    header footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Home/Home'; 
+import Home from './Home/Home';
+import Header from './common/Header/Header'; 
+import Footer from './common/Footer/Footer';
 import './App.css';
 
 function App() {
   return (
     <Router>
+      <Header /> 
+      
       <Routes>
+       
         <Route path="/" element={<Home />} />
+
+
       </Routes>
+
+      <Footer /> 
     </Router>
   );
 }
