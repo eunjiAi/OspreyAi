b3faca746e8320f38d538d73c6f6cb0c6bb23589
ieunji0116 <136577730+ieunji0116@users.noreply.github.com>
2024-10-31 16:13:37 +0900

    ss
    ss

---
+++ first/src/main/java/org/myweb/first/board/controller/ReplyController.java
@@ -0,0 +1,30 @@
+package org.myweb.first.board.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.myweb.first.board.model.service.ReplyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+@Controller
+public class ReplyController {
+    @Autowired
+    ReplyService replyService;
+
+    // 댓글, 대댓글 등록 페이지로 이동 처리용
+    @RequestMapping("breplyform.do")
+    public ModelAndView moveReplyPage(
+            ModelAndView mv,
+            @RequestParam("bnum") int boardNum,
+            @RequestParam("page") int currentPage) {
+
+        mv.addObject("bnum", boardNum);
+        mv.addObject("currentPage", currentPage);
+        mv.setViewName("board/boardReplyForm");
+
+        return mv;
+    }
+}
