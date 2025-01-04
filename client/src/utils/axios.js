7564ab62adef1e3cf703416cd3ebd2e038f34de8
ShaynePark22 <smtt22@gmail.com>
2025-01-04 14:14:47 +0900

    Update axios.js
    모든페이지에 자동으로
    Authorization
    전달되게 설정

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,11 +1,25 @@
 // src/utils/axios.js
-import axios from 'axios';
+import axios from "axios";
 
 const apiClient = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL, // .env 파일에 설정된 URL 사용
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
+
+// Authorization 헤더 추가를 위한 Axios Interceptor
+apiClient.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default apiClient;
