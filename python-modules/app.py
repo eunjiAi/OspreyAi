530af4661eef8148430bc9974b34d3ad263d4e48
ieunji0116 <136577730+ieunji0116@users.noreply.github.com>
2025-01-06 15:54:15 +0900

    설치명령어 주석 추가

--- python-modules/app.py
+++ python-modules/app.py
@@ -1,3 +1,8 @@
+# USAGE
+# conda install -c conda-forge dlib
+# pip install Flask Flask-Cors sqlalchemy pillow numpy opencv-python mediapipe pytz cx_Oracle jwt
+# pip install PyJWT
+
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import sys
