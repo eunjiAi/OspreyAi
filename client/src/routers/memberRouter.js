de1ba54b5103fd2839c5ee11b9bd21c5c3b1ae09
ShaynePark22 <smtt22@gmail.com>
2025-01-08 10:04:56 +0900

    필요없는 파일 제거

--- client/src/routers/memberRouter.js
+++ client/src/routers/memberRouter.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Route } from "react-router-dom";
 
-import Signup from "../pages/signup/Signup";
 import FaceIdRegister from "../pages/faceid/FaceIdRegister";
 import FaceIDLogin from "../pages/member/login/FaceIDLogin";
 import Login from "../pages/member/login/Login";
@@ -9,7 +8,6 @@ import FindId from "../pages/member/login/FindId";
 import FindPassword from "../pages/member/login/FindPassword";
 
 const memberRouter = [
-  <Route path="/signup" element={<Signup />} />,
   <Route path="/faceIDLogin" element={<FaceIDLogin />} />,
   <Route path="/login" element={<Login />} />,
   <Route path="/findId" element={<FindId />} />,
