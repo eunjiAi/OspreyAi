f1cf09d3854b75f5ebaca3d222b10594da48718e
ieunji0116 <136577730+ieunji0116@users.noreply.github.com>
2025-01-09 16:11:00 +0900

    관리자페이지 버튼 관리자만 보이게 수정

--- client/src/pages/mypage/MyPage.js
+++ client/src/pages/mypage/MyPage.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../AuthProvider";
 import "./MyPage.css";
 
 function MyPage() {
   const navigate = useNavigate();
   const location = useLocation();
+  const { role } = useContext(AuthContext); 
 
-  // "마이페이지"와 관련된 모든 경로 상태 확인
+
   const isMyPagePath =
     location.pathname === "/mypage" ||
-    location.pathname.startsWith("/mypage/mypageMain") || // "마이페이지 메인" 경로
-    location.pathname.startsWith("/mypage/mypageUpdate"); // "정보 수정" 경로
+    location.pathname.startsWith("/mypage/mypageMain") || 
+    location.pathname.startsWith("/mypage/mypageUpdate"); 
 
   return (
     <div className="mypage-layout">
@@ -42,12 +44,17 @@ function MyPage() {
         >
           회원탈퇴
         </button>
-        <button
-          onClick={() => navigate("/mypage/mypageAdmin")}
-          className={`tab ${location.pathname === "/mypage/mypageAdmin" ? "active" : ""}`}
-        >
-          (관리자)회원 관리
-        </button>
+
+
+        {/* 관리자인 경우에만 버튼 보이기 */}
+        {role === "ADMIN" && (
+          <button
+            onClick={() => navigate("/mypage/mypageAdmin")}
+            className={`tab ${location.pathname === "/mypage/mypageAdmin" ? "active" : ""}`}
+          >
+            (관리자)회원 관리
+          </button>
+        )}
       </div>
 
       {/* 오른쪽 페이지 */}
