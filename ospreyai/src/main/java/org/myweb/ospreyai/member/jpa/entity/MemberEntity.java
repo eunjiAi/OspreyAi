550ea87d8e5e2a5386b3cf4784f8fba5e9b17ed4
ieunji0116 <136577730+ieunji0116@users.noreply.github.com>
2025-01-09 12:23:37 +0900

    facevector제거

--- ospreyai/src/main/java/org/myweb/ospreyai/member/jpa/entity/MemberEntity.java
+++ ospreyai/src/main/java/org/myweb/ospreyai/member/jpa/entity/MemberEntity.java
@@ -18,47 +18,45 @@ import java.sql.Date;
 public class MemberEntity {
     @Id
     @Column(name = "UUID", nullable = false)
-    private String uuid;	//	UUID	VARCHAR2(36 BYTE)
+    private String uuid; // UUID VARCHAR2(36 BYTE)
     @Column(name = "NAME")
-    private String name;	//	NAME	VARCHAR2(20 BYTE)
+    private String name; // NAME VARCHAR2(20 BYTE)
     @Column(name = "NICKNAME")
-    private String nickname;	//  NICKNAME	VARCHAR2(30 BYTE)
+    private String nickname; // NICKNAME VARCHAR2(30 BYTE)
     @Column(name = "MEMBERID", nullable = false, unique = true)
-    private String memberId;	//	MEMBERID	VARCHAR2(100 BYTE)
+    private String memberId; // MEMBERID VARCHAR2(100 BYTE)
     @Column(name = "PW")
-    private String pw;		//	PW	VARCHAR2(100 BYTE)
+    private String pw; // PW VARCHAR2(100 BYTE)
     @Column(name = "PHONE_NUMBER")
-    private String phoneNumber;	//	PHONE_NUMBER	VARCHAR2(15 BYTE)
+    private String phoneNumber; // PHONE_NUMBER VARCHAR2(15 BYTE)
     @Column(name = "GENDER")
-    private String gender;	//	GENDER	CHAR(1 BYTE)
+    private String gender; // GENDER CHAR(1 BYTE)
     @Column(name = "ADMIN_YN", nullable = false, columnDefinition = "N")
-    private String adminYn;	//	ADMIN_YN	CHAR(1 BYTE)
+    private String adminYn; // ADMIN_YN CHAR(1 BYTE)
     @Column(name = "FACE_ID")
-    private String faceId;	//	FACE_ID	VARCHAR2(255 BYTE)
-    @Column(name = "FACE_VECTOR")
-    private String faceVector;  //  FACE_VECTOR	CLOB
+    private String faceId; // FACE_ID VARCHAR2(255 BYTE)
     @Column(name = "ENROLL_DATE")
-    private Date enrollDate;	//	ENROLL_DATE	DATE
+    private Date enrollDate; // ENROLL_DATE DATE
     @Column(name = "LASTMODIFIED")
-    private Date lastModified;	//	LASTMODIFIED	DATE
+    private Date lastModified; // LASTMODIFIED DATE
     @Column(name = "GOOGLE")
-    private String google;      //    GOOGLE	VARCHAR2(100 BYTE)
+    private String google; // GOOGLE VARCHAR2(100 BYTE)
     @Column(name = "NAVER")
-    private String naver;       //    NAVER	VARCHAR2(100 BYTE)
+    private String naver; // NAVER VARCHAR2(100 BYTE)
     @Column(name = "KAKAO")
-    private String kakao;       //    KAKAO	VARCHAR2(100 BYTE)
+    private String kakao; // KAKAO VARCHAR2(100 BYTE)
     @Column(name = "LOGIN_OK", nullable = false, columnDefinition = "Y")
-    private String loginOk;		//	LOGIN_OK	CHAR(1 BYTE)
+    private String loginOk; // LOGIN_OK CHAR(1 BYTE)
     @Column(name = "EMAIL")
-    private String email;       //  EMAIL	VARCHAR2(255 CHAR)
+    private String email; // EMAIL VARCHAR2(255 CHAR)
 
     @PrePersist
-    public void prePersist(){
+    public void prePersist() {
         enrollDate = new Date(System.currentTimeMillis());
         lastModified = new Date(System.currentTimeMillis());
     }
 
-    public Member toDto(){
+    public Member toDto() {
         return Member.builder()
                 .uuid(uuid)
                 .name(name)
@@ -69,7 +67,6 @@ public class MemberEntity {
                 .gender(gender)
                 .adminYn(adminYn)
                 .faceId(faceId)
-                .faceVector(faceVector)
                 .enrollDate(enrollDate)
                 .lastModified(lastModified)
                 .google(google)
